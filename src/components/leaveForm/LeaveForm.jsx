e640a1edd18240dea4049aec05271a0d754e0e52
Anas <anas241124@gmail.com>
2023-07-07 09:01:24 +0530

    first commit

--- src/components/leaveForm/LeaveForm.jsx
+++ src/components/leaveForm/LeaveForm.jsx
@@ -4,12 +4,15 @@ import { useNavigate } from 'react-router-dom';
 import { serverUrl } from '../../utils/serverUrl';
 import './leaveForm.scss';
 import { toast } from 'react-hot-toast';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const LeaveForm = () => {
   const [username, setUsername] = useState('');
   const [department, setDepartment] = useState('');
   const [leaveType, setLeaveType] = useState('');
-  const [duration, setDuration] = useState(1);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   const [users, setUsers] = useState([]);
   const [localStorageUser, setLocalStorageUser] = useState('');
   const [loading, setLoading] = useState(false);
@@ -48,8 +51,25 @@ const LeaveForm = () => {
     }
   };
 
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+  };
+
+  const calculateDurationInDays = () => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const durationInDays = Math.floor((end - start) / (1000 * 60 * 60 * 24)) + 1;
+    return durationInDays;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const duration = calculateDurationInDays();
 
     const leaveData = {
       username,
@@ -109,13 +129,32 @@ const LeaveForm = () => {
             </div>
             <br />
             <div className="input">
-              <label>Duration (in days):</label>
-              <input
-                type="number"
-                min="1"
-                value={duration}
-                onChange={(e) => setDuration(parseInt(e.target.value))}
+              <label>Start Date:</label>
+              <DatePicker
+                selected={startDate}
+                onChange={handleStartDateChange}
+                dateFormat="dd/MM/yyyy"
+                showYearDropdown
+                showMonthDropdown
+                dropdownMode="select"
               />
+            </div>
+            <br />
+            <div className="input">
+              <label>End Date:</label>
+              <DatePicker
+                selected={endDate}
+                onChange={handleEndDateChange}
+                dateFormat="dd/MM/yyyy"
+                showYearDropdown
+                showMonthDropdown
+                dropdownMode="select"
+              />
+            </div>
+            <br />
+            <div className="input">
+              <label>Duration (in days):</label>
+              <input type="number" value={calculateDurationInDays()} disabled />
             </div>
             <br />
             <button type="submit" disabled={loading}>
